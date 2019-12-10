ba1c87f3ea8cc248ec8a5318c3140af5c89e8abf
Mrinmayi <mrinmayi.17@gmail.com>
2019-12-10 16:43:06 -0600

    Started script to make counterbalances for scene replication

---
+++ Experiment5/BehaviouralData/TE_Exp5_MakeCounterbalance.R
@@ -0,0 +1,135 @@
+library(openxlsx)
+library(reshape)
+library(plyr)
+#library(dplyr)
+
+########################## Set Admin variables ##########################
+
+#What all to do right now
+Make=1
+Check=0
+Save=1
+FinalCB <- data.frame()
+
+BasePath <- "/Users/mrinmayi/GoogleDrive/Mrinmayi/Research/TemporalExpectation/Experiment/"
+
+
+#List of counterbalances with lists assigned to conditions in the order given in CondNames
+ListRot <- list("CondNames"=list("TR"=list("TR_Old", "TR_New"),
+                                 "TI"=list("TI_Old", "TI_New")),
+                "CB1"=list("TR"=c(1, 2),
+                           "TI"=c(3, 4)),
+                "CB2"=list("TR"=c(2, 3),
+                           "TI"=c(4, 1)),
+                "CB3"=list("TR"=c(3, 4),
+                           "TI"=c(1, 2)),
+                "CB4"=list("TR"=c(4, 1),
+                           "TI"=c(2, 3)))
+
+
+
+################################ Sanity check functions ################################
+#Make sure that conditions and categories are evenly spread out across sets. To make sure that not all
+#4 objects in a set are eventually in the Similar condition, or that not all of the objects are Tools 
+#etc.
+#CountRows <- function(df, Col){
+  count(df[, Col])
+}
+#CheckCB <- function(df=NULL, Build=1) {
+  if(Build==1){ #This is initially building the CB
+    Check <- list()
+    Checked <- 0
+    #Condition per ISI is also controlled in the excel sheet
+    Check$CatPerISI <- ddply(df, c("ISIType"), CountRows, "Category")
+    Check$CatPerSet <- ddply(df, c("Set"), CountRows, "Category")
+    Check$CondPerSet <- ddply(df, c("Set"), CountRows, "Condition")
+    Check$OldPerThirds <- ddply(df[df$Condition=="Old",], c("Thirds"), CountRows, "Condition")
+    Check$SimPerThirds <- ddply(df[df$ListType=="Similar",], c("Thirds"), CountRows, "Condition")
+    Checked <- ifelse(any(any(Check$CatPerISI$freq>5),
+                          any(Check$CatPerSet$freq>=4),
+                          any(Check$CondPerSet$freq>=4),
+                          any(Check$CondPerThirds$freq>=8),
+                          any(Check$OldPerThirds$freq>=18),
+                          any(Check$SimPerThirds$freq>=10)), 1, 0)
+    return(list(Checked, Check))
+  }else if(Build==2){ #Only need to make sure here that the ISI are equally split across conditions in the first ISIs
+    #Everything else is taken care of above
+    CheckTIISI <- list()
+    CheckedTIISI <- 0
+    df <- df[df$NumPres==1,]
+    CheckTIISI$CondPerISI <- ddply(df, c("ISI"), count, "Condition")
+    CheckedTIISI <- ifelse(any(any(CheckTIISI$CondPerISI$freq>=13)), 1, 0)
+    return(list(CheckedTIISI, CheckTIISI))
+  }
+  
+}
+#CheckCB() #initialise
+
+#Make sure that after the copying and sorting the trials, the same 4 objects are presented one after another
+CheckRepetitions <- function(df){
+  if(all(df[1:4, "Items"] == df[5:8, "Items"])){
+    Good <- 1
+  }
+  else if(!(all(df[1:4, "Items"] == df[5:8, "Items"]))){
+    stop("Order of items in consecutive presentations is not the same. Investigate!!!!!")
+  }
+  return(Good)
+}
+
+#Randomise ISIs in the TI block either similar to Thavabalasingham et al., 2017 or like Debbie's suggestion
+RandomiseTI <- function(df, TIType){
+  #Some jugaad required here. We want to make sure the subsequent presentations of the TI objects are paired with
+  #opposite ISIs. Since we're now changing around the order of the ISIs (See ISICombo variable), we no longer know
+  #which are the shortest and longest ISIs
+  ShortISI <- order(ISICombo)[1:2]
+  LongISI <- order(ISICombo)[3:4]
+  
+  #First randominse the set of  trials
+  df[1:4, "ISIType"] <- sample(4)
+  #df[5:8, "ISIType"] <- sample(4)
+  
+  ##### Use this if you want to randomise the TI block exactly like in Thavabalasingham et al., 2017 like 
+  if(TIType=="Rand"){
+    MeanISI <- ISICombo #jittered around whatever is the ISIcombo for this participant
+    SDISI <- c(TIJitters[[as.character(MeanISI[1])]],
+               TIJitters[[as.character(MeanISI[2])]],
+               TIJitters[[as.character(MeanISI[3])]],
+               TIJitters[[as.character(MeanISI[4])]])
+    
+    RandISI <- c(0, 0, 0, 0)
+    #Set ISIs. Just make sure a negative ISI isn't returned
+    while(any(RandISI<=17)){
+      for(i in 1:4){
+        RandISI[i] <- round(rnorm(1, mean=MeanISI[df[i, "ISIType"]], sd=SDISI[df[i, "ISIType"]]), digits=0)
+      }
+    }
+    df[1:4, "ISI"] <- RandISI
+    df[5:8, "ISI"] <- RandISI[sample(4)]
+  }
+  ##### For the time being, using the Debbie version where the trials in a set that were short ISIs in
+  ##### the first presentation, should become the long ISIs in the second presentation 
+  else if(TIType=="Shuffle"){
+    df[which(row.names(df)==5:8 & df[1:4, "ISIType"] %in% ShortISI), "ISIType"] <- LongISI[sample(1:2)]
+    df[which(row.names(df)==5:8 & df[1:4, "ISIType"] %in% LongISI), "ISIType"] <- ShortISI[sample(1:2)]
+    
+    df[df$ISIType==1, "ISI"] <- ISICombo[1]
+    df[df$ISIType==2, "ISI"] <- ISICombo[2]
+    df[df$ISIType==3, "ISI"] <- ISICombo[3]
+    df[df$ISIType==4, "ISI"] <- ISICombo[4]
+  }
+  return(df)
+}
+
+
+################################ Read in files ################################
+
+#Read in the master sheet that has all of the original details for how each participant should be organised
+MasterList <- read.xlsx(paste(BasePath, "Experiment5/Counterbalancing/Counterbalancing_MasterSheet_Exp5.xlsx", sep=""), sheet="ListAssignment", 
+                        cols=1:3, colNames=TRUE)
+
+ISIComboDict <- read.xlsx(paste(BasePath, "Experiment5/Counterbalancing/Counterbalancing_MasterSheet_Exp5.xlsx", sep=""), sheet="ISIRotation", 
+                          cols=1:5, rows=2:26, colNames=TRUE)
+
+#Change this to 1, 2, 3 and so on and so forth for different participants
+Part=1
+
